9540e58d0f1c9dc852875d9fbb7609279e6985e1
julia <y.trubitsyna@g.nsu.ru>
2020-12-02 01:41:39 +0700

    Delete googletests

--- lab1_tests/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/programming/prog2_1/lab1_tests/lib/googletest-master
-# Build directory: D:/programming/prog2_1/lab1_tests/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- lab1_tests/cmake-build-debug/lib/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/programming/prog2_1/lab1_tests/lib/googletest-master/googletest
-# Build directory: D:/programming/prog2_1/lab1_tests/cmake-build-debug/lib/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
